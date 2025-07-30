71534a71a623175a12260fadae134c81cdf70c98
Darshan sonavane <dardarshansonavane143@gmail.com>
2025-07-30 21:24:44 +0530

    I push the 1st and 2nd program

---
+++ TestjavaProject1/src/packagetest/Firstprogram.java
@@ -0,0 +1,10 @@
+package packagetest;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("This is  first Program");
+
+	}
+
+}
---
+++ TestjavaProject1/src/packagetest/Secondprogram.java
@@ -0,0 +1,9 @@
+package packagetest;
+
+public class Secondprogram {
+
+	public static void main(String[] args) {
+		System.out.println("This is Secon Program");
+	}
+
+}
